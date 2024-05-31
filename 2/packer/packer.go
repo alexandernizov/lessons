bb2c1ddb1946a35aaf0fabd5a6adc837ac6e2bc4
Alexander <alexander@MacBook-Pro-Alexander.local>
2024-05-31 15:24:37 +0300

    add builder

--- 2/packer/packer.go
+++ 2/packer/packer.go
@@ -39,3 +39,38 @@ func UnpackString(input string) (string, error) {
 	res = res + prev
 	return res, nil
 }
+
+// Через билдер строк - работает в 3 раза быстрее
+func UnpackString2(input string) (string, error) {
+	// Алгоритм:
+	// 1. Пустая строка - пустая строка
+	// 2. строка не начинается с цифры
+	// 3. Разделяем строку по рунам
+	// 4. Добавляем в буфер новые строчки, через repeat
+
+	if input == "" {
+		return "", nil
+	}
+
+	b := strings.Builder{}
+	var prev rune = -1
+	for _, let := range input {
+		if unicode.IsDigit(let) {
+			if prev == -1 {
+				return "", ErrIncorrectInput
+			}
+			multiply := int(let - '0')
+			for i := 0; i < int(multiply); i++ {
+				b.WriteRune(prev)
+			}
+			prev = -1
+			continue
+		}
+		if prev != -1 {
+			b.WriteRune(prev)
+		}
+		prev = let
+	}
+	b.WriteRune(prev)
+	return b.String(), nil
+}
