7fd1ef0fde3991885c267c8f9724ca85c6c2861b
alexandernizov <alexandernizov@icloud.com>
2024-06-06 09:18:26 +0300

    some changes

--- 3.2/collections/deque.go
+++ 3.2/collections/deque.go
@@ -1,42 +1,10 @@
 package collections
 
 type Dueue struct {
-	c Collection
+	Collection
 }
 
 func NewDeque() *Dueue {
 	c := New()
-	return &Dueue{c: *c}
-}
-
-func (s *Dueue) PushBack(value any) {
-	s.c.PushBack(value)
-}
-
-func (s *Dueue) PopBack() (any, bool) {
-	return s.c.PopBack()
-}
-
-func (s *Dueue) PeekBack() (any, bool) {
-	return s.c.PeekBack()
-}
-
-func (s *Dueue) PushFront(value any) {
-	s.c.PushFront(value)
-}
-
-func (s *Dueue) PopFront() (any, bool) {
-	return s.c.PopFront()
-}
-
-func (s *Dueue) PeekFront() (any, bool) {
-	return s.c.PeekFront()
-}
-
-func (d *Dueue) Contains(value any) bool {
-	return d.c.Contains(value)
-}
-
-func (d *Dueue) Clear() {
-	d.c.Clear()
+	return &Dueue{*c}
 }
